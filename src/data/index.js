e86a3ca429fbac681bc112bf5275f7004d6343ff
Henrique <henriqueaureliocsilva@gmail.com>
2021-12-06 13:49:35 -0300

    feat: users crud

--- src/data/index.js
+++ src/data/index.js
@@ -2,14 +2,14 @@ import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient();
 
-prisma.users
-  .findMany({
-    where: {
-      email: "henrique@dev.com",
-    },
-  })
-  .then((docs) => console.log(docs))
-  .catch((err) => console.log(err));
+// prisma.users
+//   .findMany({
+//     where: {
+//       email: "henrique@dev.com",
+//     },
+//   })
+//   .then((docs) => console.log(docs))
+//   .catch((err) => console.log(err));
 
 // try {
 //   const docs = await prisma.users.findMany({
