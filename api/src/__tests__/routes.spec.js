57df08898069a8c997bb0b74e2b338ad23048009
Henrique <henriqueaureliocsilva@gmail.com>
2022-02-06 21:03:01 -0300

    test: unitary test if logged in is successfull

--- api/src/__tests__/routes.spec.js
+++ api/src/__tests__/routes.spec.js
@@ -1,9 +1,15 @@
+import bcrypt from 'bcrypt'
 import request from 'supertest'
+
+import { prisma } from '~/data'
 import { app } from '~/server-setup.js'
 
 const server = app.listen()
 
 describe('User routes', () => {
+  beforeAll(async () => {
+    await prisma.user.deleteMany({})
+  })
   it('should return not found with wrong password', async () => {
     //preparacao
     const email = 'henrique@dev.com'
@@ -23,5 +29,25 @@ describe('User routes', () => {
     //expectativa
 
     expect(result.status).toBe(404)
+  })
+  it('should return logged in user by correct credentials', async () => {
+    //preparacao
+    const email = 'henrique2@dev.com'
+    const password = '123456'
+    const saltRounds = 10
+
+    const hashedPassword = await bcrypt.hash(password, saltRounds)
+
+    await prisma.user.create({ data: { email, password: hashedPassword } })
+    //execucao
+    const result = await request(server).get('/login').auth(email, password)
+    //expectativa
+    console.log(result.body)
+    expect(result.status).toBe(200)
+    expect(result.body).toBeTruthy()
+    expect(result.body.user.id).toBeTruthy()
+    expect(result.body.user.email).toBe(email)
+
+    // expect(result.body.user.password).toBeFalsy()
   })
 })
