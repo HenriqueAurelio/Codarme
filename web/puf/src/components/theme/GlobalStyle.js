51fe8932b72ef2f51d46af4f94c99e4f48a3dbac
Henrique <henriqueaureliocsilva@gmail.com>
2023-01-11 11:25:54 -0300

    feat: custom fonts

--- web/puf/src/components/theme/GlobalStyle.js
+++ web/puf/src/components/theme/GlobalStyle.js
@@ -12,7 +12,7 @@ export const GlobalStyle = createGlobalStyle`
     ${font}
 
     margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    font-family: Manrope,-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
       'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
       sans-serif;
     -webkit-font-smoothing: antialiased;
